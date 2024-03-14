72312dcaf1534126dcfe2209cc84b0d549d34fef
Ben Walker <forks.carbons.0w@icloud.com>
2024-03-15 12:46:56 +1300

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,11 @@ export default function Home() {
   );
   const [screenResolution, setScreenResolution] = useState<string | null>(null);
   const [aspectRatio, setAspectRatio] = useState<string | null>(null);
+  const [operatingSystem, setOperatingSystem] = useState<string | null>(null);
+  const [browser, setBrowser] = useState<string | null>(null);
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
+  const [location, setLocation] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchDeviceInfo = async () => {
@@ -33,14 +38,20 @@ export default function Home() {
         const height = window.screen.height;
         setScreenWidth(width);
         setScreenHeight(height);
+
         setScreenOrientation(window.screen.orientation.type);
 
         // Device Pixel Ratio
         setDevicePixelRatio(window.devicePixelRatio);
+        const physicalWidth = window.screen.width * window.devicePixelRatio;
+        const physicalHeight = window.screen.height * window.devicePixelRatio;
+        console.log(`screen resolution: ${physicalWidth}x${physicalHeight}`);
 
         // Navigator Information
         setBrowserLanguage(navigator.language);
         setBrowserOnlineStatus(navigator.onLine);
+        setOperatingSystem(window.navigator.userAgentData.platform);
+        setBrowser(window.navigator.userAgentData.brands[2].brand);
 
         // User Agent Information
         setUserAgent(navigator.userAgent);
@@ -79,6 +90,51 @@ export default function Home() {
         );
         setScreenResolution(`${width}x${height}`);
         setAspectRatio(closestRatio.name);
+
+        // Geolocation
+        if (navigator.geolocation) {
+          console.log("Geolocation is supported by your browser");
+
+          // Request permission to access the user's location
+          await navigator.geolocation.getCurrentPosition(
+            async function (position) {
+              setLatitude(position.coords.latitude);
+              setLongitude(position.coords.longitude);
+
+              // Use the latitude and longitude to get the user's location
+              const locationUrl = `https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${position.coords.latitude}&lon=${position.coords.longitude}&zoom=18&addressdetails=1`;
+              const response = await fetch(locationUrl);
+              const data = await response.json();
+              if (data && data.address) {
+                // Construct a more detailed address string
+                let detailedAddress = "";
+                if (data.address.house_number) {
+                  detailedAddress += data.address.house_number + " ";
+                }
+                if (data.address.road) {
+                  detailedAddress += data.address.road + ", ";
+                }
+                if (data.address.suburb) {
+                  detailedAddress += data.address.suburb + ", ";
+                }
+                if (data.address.city) {
+                  detailedAddress += data.address.city + ", ";
+                }
+                if (data.address.country) {
+                  detailedAddress += data.address.country;
+                }
+                setLocation(detailedAddress.trim());
+              } else {
+                console.log("Unable to determine location");
+              }
+            },
+            function (error) {
+              console.error("Error occurred: " + error.message);
+            }
+          );
+        } else {
+          console.log("Geolocation is not supported by your browser");
+        }
       } catch (error) {
         console.error("Error fetching device info:", error);
       } finally {
@@ -142,6 +198,26 @@ export default function Home() {
           <div className={styles.childContainer}>
             <p>Vibration Supported:</p>
             <p>{vibrationSupported ? "Yes" : "No"}</p>
+          </div>
+          <div className={styles.childContainer}>
+            <p>Operating System:</p>
+            <p>{operatingSystem}</p>
+          </div>
+          <div className={styles.childContainer}>
+            <p>Browser:</p>
+            <p>{browser}</p>
+          </div>
+          <div className={styles.childContainer}>
+            <p>Latitude:</p>
+            <p>{latitude}</p>
+          </div>
+          <div className={styles.childContainer}>
+            <p>Longitude:</p>
+            <p>{longitude}</p>
+          </div>
+          <div className={styles.childContainer}>
+            <p>Location:</p>
+            <p>{location}</p>
           </div>
         </div>
       )}
