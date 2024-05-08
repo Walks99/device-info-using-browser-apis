dc890a844fa9287e32bd44e4367381cd8aa25245
Ben Walker <forks.carbons.0w@icloud.com>
2024-05-08 13:43:15 +1200

    Adding microsft clarity script

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import React from "react";
 import styles from "./page.module.scss";
+import Script from 'next/script';
+
+const clarityScript = `
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "m8kagekqmr");
+`;
 
 export default function landingPage() {
   return (
@@ -20,6 +29,7 @@ export default function landingPage() {
           </li>
         </ul>
       </div>
+      <Script id="ms-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: clarityScript }} />
     </div>
   );
 }
