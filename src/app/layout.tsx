d6d58c602a07be6331a5b8465a9d1a02a910353a
Ben Walker <forks.carbons.0w@icloud.com>
2024-05-08 14:25:00 +1200

    Moved microsft clarity tracking code to the layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Script from 'next/script';
+
+const clarityScript = `
+(function(c,l,a,r,i,t,y){
+  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "m8kagekqmr");
+`;
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script id="ms-clarity" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: clarityScript }} />
     </html>
   );
 }
